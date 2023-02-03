1fd95dc276b06fd25471429073ed7450823d5bbd
Muhammad Waqar Irshad <khan.waqar278@gmail.com>
2023-02-03 12:49:29 +0500

    fix picture

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Muhammad Waqar Irshad',
     title: "Web Developer",
     desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
-    image: './waqar.jpg',
+    image: 'https://raw.githubusercontent.com/khanwaqar/waqar-irshad-portfolio/main/public/waqar.jpg',
     resumePdf: resume
 }
