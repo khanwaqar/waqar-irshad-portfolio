5d15de09cdcffaadbdffdc1be54e086f3b18f656
waqar <khan.waqar278@gmail.com>
2023-12-08 13:46:39 +0500

    contact updated

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'khan.waqar278@gmail.com',
-    phone: '+92-335-1651992',
+    phone: '+92-314-1813697',
     address: 'House P1630-E, Street 2, Eidgah, Rawalpindi, Pakistan',
 
     sheetAPI: ''
