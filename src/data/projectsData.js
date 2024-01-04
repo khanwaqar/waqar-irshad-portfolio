e10c2ff2032ad547494fec76bd781e39efd3a821
waqar <khan.waqar278@gmail.com>
2024-01-04 15:45:55 +0500

    newprojectadded

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,7 +2,25 @@
 
 export const projectsData = [
     {
+        id: 5,
+        projectName: 'Zimmo - Data Quality',
+        projectDesc: 'This is the second largest Real Estate Portal in Belgium, We have worked on scrapping the Data and capturing the data from multiple Data sources',
+        tags: ['PHP', 'DOM', 'Symfony', 'MySQL'],
+        code: '',
+        demo: 'https://zimmo.be',
+        image: "https://img.static-rmg.be/a/view/q75/w1780/h875/5262255/capture-decran-2023-05-10-151553-jpg.jpg"
+    },
+    {
         id: 1,
+        projectName: 'Dupatta Store Pakistan',
+        projectDesc: 'This is a full E-Commerce Project which include frontennd and Backend. basically this project manage whole ecommerce business model shop at frontend and admin-panel at backend',
+        tags: ['Laravel', 'PHP' ,'HTML', 'CSS', 'Bootstrap','Javascript'],
+        code: '',
+        demo: 'https://dupattastore.pk/',
+        image: "https://dupattastore.pk/storage/files/1/Shop%20Products/Dupatta%20Store.png"
+    },
+    {
+        id: 6,
         projectName: 'Weather App',
         projectDesc: 'This project seeks to explore weather Data',
         tags: ['React', 'HTML' ,'CSS', 'Javascript'],
@@ -37,15 +55,7 @@ export const projectsData = [
         demo: '',
         image: "https://qph.cf2.quoracdn.net/main-qimg-3402ee04f534f4fb30516c885004bfb6-lq"
     },
-    {
-        id: 5,
-        projectName: 'Zimmo - Data Quality',
-        projectDesc: 'This is the second largest Real Estate Portal in Belgium, We have worked on scrapping the Data and capturing the data from multiple Data sources',
-        tags: ['PHP', 'DOM', 'Symfony', 'MySQL'],
-        code: '',
-        demo: 'https://zimmo.be',
-        image: "https://pbs.twimg.com/card_img/1730507562024316928/I7I3AfM3?format=jpg&name=small"
-    },
+    
     
 ]
 
@@ -54,6 +64,7 @@ export const projectsData = [
 // Leave it blank instead as shown below
 
 /* 
+
 {
     id: 1,
     projectName: 'Car Pooling System',
